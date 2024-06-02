933790214363c6ec7315dac9ba577e4b16155319
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-06-02 20:18:33 +0300

    rename

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -11,8 +11,16 @@ export default function ImageModal({ onRequestClose, isOpen, fullSrc, alt }) {
           backgroundColor: 'rgba(0, 0, 0, 0.75)',
         },
         content: {
+          width: '80vw',
+          height: '80vh',
           padding: '0',
           overflow: 'hidden',
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
         },
       }}>
       <img src={fullSrc} alt={alt} className={css.modalImg} />
